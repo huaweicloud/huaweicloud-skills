6403da88121754cf29a44454b3ba3e000f38ef95
lqq-60109515 <lqq-60109515@noreply.gitcode.com>
2026-06-03 17:43:29 +0800

    Modify flexus-l-manage skill according to specifications

---
+++ skills/computing/flexus-l/huawei-cloud-flexus-l-server-manage/scripts/config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Shared configuration for Flexus L lifecycle management
+"""
+
+# Region configuration (from official documentation)
+REGIONS = {
+    'cn-north-4': {'name': '华北-北京四', 'short': 'beijing'},
+    'cn-south-1': {'name': '华南-广州', 'short': 'guangzhou'},
+    'cn-east-3': {'name': '华东-上海一', 'short': 'shanghai'},
+    'cn-southwest-2': {'name': '西南-贵阳一', 'short': 'guizhou'},
+    'ap-southeast-1': {'name': '中国-香港', 'short': 'hongkong'},
+    'ap-southeast-3': {'name': '亚太-新加坡', 'short': 'singapore'},
+}
